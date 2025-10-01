02305d7c6004a955e6194bad6bebfe6f056ec615
Jake Barbagallo <jakebarbagallo@gmail.com>
2025-10-01 15:41:36 -0400

    added tutorial

---
+++ CounterShot.unity/Counter-Shot/Assets/Scripts/Parryablebullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parryablebullet : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        rb.velocity= transform.position*speed;
+     
+        StartCoroutine(Destroyself());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Destroyself()
+    {
+        yield return new WaitForSeconds(6);
+        Destroy(gameObject);
+    }
+}
