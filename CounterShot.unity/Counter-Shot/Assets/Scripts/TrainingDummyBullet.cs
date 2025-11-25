7cb5d7ec9dd8e340271b8d675d60c893b1213073
Em Sheetz <E4Endermen@gmail.com>
2025-11-25 00:57:13 -0500

    added a tutorial scene

---
+++ CounterShot.unity/Counter-Shot/Assets/Scripts/TrainingDummyBullet.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingDummyBullet : MonoBehaviour
+{
+    public float speed = 8;
+    Rigidbody2D rb;
+    CircleCollider2D cd;
+    CapsuleCollider2D cc;
+    [SerializeField] Animator parryexplode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        parryexplode = GetComponent<Animator>();
+        rb.velocity = transform.position * speed;
+        cd = GetComponent<CircleCollider2D>();
+        cc = GetComponent<CapsuleCollider2D>();
+        StartCoroutine(Destroyself());
+    }
+
+    IEnumerator Destroyself()
+    {
+        yield return new WaitForSeconds(6);
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("collision is we parring animatoring");
+        if (collision.tag == "parryCircle")
+        {
+            //Debug.Log(collision);
+            parryexplode.SetTrigger("isParried");
+        }
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void OnAnimStart()
+    {
+        rb.velocity = transform.position * 0;
+        if (cc == null) cd.enabled = false; //circle colider
+        if (cd == null) cc.enabled = false; //capsule collider
+
+    }
+    public void OnAnimFinishDestroy()
+    {
+        //StartCoroutine(PoopieChudWaitForSec());
+        Destroy(gameObject);
+    }
+}
