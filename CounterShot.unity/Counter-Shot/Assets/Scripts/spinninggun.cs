0e2649b705cd452255dad49e8a1531568f04b1b5
Em Sheetz <E4Endermen@gmail.com>
2025-12-01 16:37:20 -0500

    stuff i did over break

--- CounterShot.unity/Counter-Shot/Assets/Scripts/spinninggun.cs
+++ CounterShot.unity/Counter-Shot/Assets/Scripts/spinninggun.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class spinninggun : MonoBehaviour
 {
     [SerializeField] float rotationspeed;
+    [SerializeField] Sprite parryableBulletSprite;
     public GameObject Bullet;
     int parrychance;
 
@@ -31,6 +32,8 @@ public class spinninggun : MonoBehaviour
         { 
              SpriteRenderer sprite = spawnedbullet.GetComponent<SpriteRenderer>();
             sprite.color = new UnityEngine.Color(1f, 0f, 0.82f);
+            //FIGURE OUT HOW TO SET SPRITE TO PINK ONE HERE
+            //spawnedbullet.GetComponent<SpriteRenderer>().
             spawnedbullet.tag = "Parryable";
             spawnedbullet.AddComponent<Parryablebullet>(); //adds parryable bullet script to the obj
         }
