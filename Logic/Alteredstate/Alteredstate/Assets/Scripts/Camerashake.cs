dec4440224a76fc515130f927dfbc03e2a432aed
Jake Barbagallo <jakebarbagallo@gmail.com>
2026-04-06 16:01:40 -0400

    juice

---
+++ Logic/Alteredstate/Alteredstate/Assets/Scripts/Camerashake.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class camerashake : MonoBehaviour
+{
+    public static camerashake instance;
+    private void Awake() => instance = this;
+
+    void onshake(float duration, float strength)
+    {
+        transform.DOShakePosition(duration, strength);
+        transform.DOShakeRotation(duration, strength);
+    }
+
+    public static void shake(float duration, float strength) => instance.onshake(duration, strength);
+}
